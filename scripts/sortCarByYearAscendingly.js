149ff898afcb1203823d9524b47660d2df44d2d7
cndystf <88188548+cndystf@users.noreply.github.com>
2022-03-10 22:45:20 +0700

    Add files via upload

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,23 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars]; //cuma kliatan isinya array
+
+  let x
+  let y
+  for (x=0;x<cars.length;x++) { 
+    for (y=0;y<cars.length-x-1;y++) { 
+      if (result[y].year > result[y+1].year) {
+        let temp = result[y]
+        result[y] = result [y+1]
+        result[y+1] = temp
+      }
+  }
+}
+
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
